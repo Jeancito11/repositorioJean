2181be9381fb3badfe99470f5cff0e7ab7102f60
Jeancarlo Querevalu Navarro <querevalujeancarlos85@gmail.com>
2025-04-15 12:20:08 -0500

    actualizar

--- llamadaFuncion.js
+++ llamadaFuncion.js
@@ -1,8 +1,41 @@
 const modulo=require("./calcularIMC")
 
+
+
 const pesoUsuario= 80;
-const alturaUsuario = 1.55;
+
+const alturaUsuario= 1.54;
 
 const imcUsuario = modulo.moduloCalcularIMC(pesoUsuario,alturaUsuario);
 
-console.log("El valor del IMC es:" +imcUsuario.toFixed(2));
+
+
+//imcUsuario.toFixed(2);
+
+if (imcUsuario < 18.5)
+
+  {
+
+    console.log("El valor del IMC es :" + imcUsuario.toFixed(2) + "se considera peso insuficiente");
+
+}
+
+else if (imcUsuario >=18.5 && imcUsuario <= 24.9) {
+
+  console.log("El valor del IMC es :" + imcUsuario.toFixed(2) + "se considera saludable");
+
+}
+
+else if (imcUsuario >=25 && imcUsuario <= 29.9){
+
+  console.log("El valor del IMC es :" + imcUsuario.toFixed(2) + "se considera obesidad");
+
+}
+
+  else
+
+  {
+
+   console.log("el valor del imc es: "+ imcUsuario.toFixed(2) + " Tienes obesidad.");
+
+  }
